980df1b266e049ae58cb1de4ca40a51decd99b79
Jon Sagara <jon@sagarasoftware.com>
2018-01-03 09:59:21 -0800

    Search with boosted download count, custom analyzers, and filters.

--- src/NuSearch.Indexer/Program.cs
+++ src/NuSearch.Indexer/Program.cs
@@ -35,7 +35,7 @@ namespace NuSearch.Indexer
 		static void IndexDumps()
 		{
 			Console.WriteLine("Setting up a lazy xml files reader that yields packages...");
-			var packages = DumpReader.GetPackages().Take(1000);
+			var packages = DumpReader.GetPackages();//.Take(1000);
 
 			Console.Write("Indexing documents into Elasticsearch...");
 			var waitHandle = new CountdownEvent(1);
@@ -62,15 +62,79 @@ namespace NuSearch.Indexer
 
 		private static void CreateIndex()
 		{
+			const string NUGET_ID_TOKENIZER = "nuget-id-tokenizer";
+			const string NUGET_ID_WORDS = "nuget-id-words";
+			const string NUGET_ID_ANALYZER = "nuget-id-analyzer";
+			const string NUGET_ID_KEYWORD = "nuget-id-keyword";
+
 			Client.CreateIndex(CurrentIndexName, i => i
 				.Settings(s => s
 					.NumberOfShards(2)
 					.NumberOfReplicas(0)
+					.Analysis(analysis => analysis
+						.Tokenizers(tokenizers => tokenizers
+							// Standard tokenizer doesn't split on ".", so we'll use a custom one that splits on any 
+							//   non-word character.
+							.Pattern(NUGET_ID_TOKENIZER, p => p.Pattern(@"\W+"))
+						)
+						.TokenFilters(tokenFilters => tokenFilters
+							// Customize the WordDelimiterTokenFilter to split on case change. e.g., ShebangModule will 
+							//   be cut up into Shebang and Module and because we tell it to preserve the original 
+							//   token, ShebangModule is kept as well.
+							.WordDelimiter(NUGET_ID_WORDS, w => w
+								.SplitOnCaseChange()
+								.PreserveOriginal()
+								.SplitOnNumerics()
+								.GenerateNumberParts(false)
+								.GenerateWordParts()
+							)
+						)
+						.Analyzers(analyzers => analyzers
+							// Here, we create an analyzer named nuget-id-analyzer which will split the input using our 
+							//   nuget-id-tokenizer and then filter the resulting terms using our nuget-id-words filter, 
+							//   finally we use the in-built lowercase token filter to replace all the terms with their 
+							//   lowercase counterparts.
+							.Custom(NUGET_ID_ANALYZER, c => c
+								.Tokenizer(NUGET_ID_TOKENIZER)
+								.Filters(NUGET_ID_WORDS, "lowercase")
+							)
+							// Here we create a special nuget-id-keyword analyzer. The built in Keyword Tokenizer emits 
+							//   the provided text as a single term, unchanged (IOW, it's a no-op). We then use the 
+							//   lowercase filter to lowercase the id as a whole. This will allow us to boost exact 
+							//   matches on id higher, without the user having to know the correct casing.
+							.Custom(NUGET_ID_KEYWORD, c => c
+								.Tokenizer("keyword")
+								.Filters("lowercase")
+							)
+						)
+					)
 				)
 				.Mappings(m => m
 					.Map<Package>(map => map
 						.AutoMap()
 						.Properties(ps => ps
+							.Text(s => s
+								// Here we setup our Id property as a multi field mapping. This allows us to analyze the 
+								//   Id property in a number of different ways. It will index the terms from the analysis 
+								//   chain into the inverted index, and allow us to target the different methods in which 
+								//   Id property has been analyzed at search time.
+								// At index time, id will use our nuget-id-analyzer to split the NuGet package Id into the 
+								//   proper terms. When we do a match query on the id field, Elasticsearch will perform 
+								//   analysis on the query input using our nuget-id-analyzer to generate the correct terms 
+								//   from the provided input.
+								// When we query id.keyword using the match query, Elasticsearch will simply use the whole 
+								//   query, lowercase it, and use that as the search term in the id.keyword field within 
+								//   the inverted index.
+								// Elasticsearch also creates a field in the inverted index called id.raw. This is the raw 
+								//   value which has not been analyzed. This field is well suited for sorting and 
+								//   aggregations (some locales sort with case sensitivity).
+								.Name(p => p.Id)
+								.Analyzer(NUGET_ID_ANALYZER)
+								.Fields(f => f
+									.Text(p => p.Name("keyword").Analyzer(NUGET_ID_KEYWORD))
+									.Keyword(p => p.Name("raw"))
+								)
+							)
 							.Nested<PackageVersion>(n => n
 								.Name(p => p.Versions.First())
 								.AutoMap()
--- src/NuSearch.Web/Controllers/SearchController.cs
+++ src/NuSearch.Web/Controllers/SearchController.cs
@@ -16,8 +16,41 @@ namespace NuSearch.Web.Controllers
 	    [HttpGet]
         public IActionResult Index(SearchForm form)
         {
-			// TODO: You'll write most of the implementation here
-			var model = new SearchViewModel();
+			var result = _client.Search<Package>(s => s
+				.Size(25)
+				.Query(q => q
+					.FunctionScore(fs => fs
+						// Use download count to boost a document's score, but cap this boost at 50 so that packages with
+						//   a download count over 500k don't differentiate on downloadCount.
+						.MaxBoost(50)
+						.Functions(ff => ff
+							.FieldValueFactor(fvf => fvf
+								.Field(p => p.DownloadCount)
+								.Factor(0.0001)
+							)
+						)
+						.Query(query => query
+							.MultiMatch(m => m
+								.Fields(f => f
+									// For searching on "id.keywrod", which we defined in the index (i.e., id has multi-field mapping).
+									.Field(p => p.Id.Suffix("keyword"), 1.5)
+									.Field(p => p.Id, 1.5)
+									.Field(p => p.Summary, 0.8)
+								)
+								.Operator(Operator.And)
+								.Query(form.Query)
+							)
+						)
+					)
+				)
+			);
+
+			var model = new SearchViewModel
+			{
+				Hits = result.Hits,
+				Total = result.Total,
+				Form = form
+			};
 
 	        return View(model);
 		}
